b0168d1cc6a9b65cb769c26bcef5d49015c81a4f
Mateusz Wende <matewen129@student.polsl.pl>
2020-04-01 14:33:09 +0200

    Add .env with example structure

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { Provider } from "react-redux";
 import configureStore from "./state";
 import { BrowserRouter as Router } from "react-router-dom";
 
+// Load environment variables
+require("dotenv").config();
+
 const initialState = (window as any).initialReduxState;
 const store = configureStore(initialState);
 
