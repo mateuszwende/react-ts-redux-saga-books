867d9a5156a62bd787625a3be01efee978c5adb0
Mateusz Wende <matewen129@student.polsl.pl>
2020-04-01 16:08:34 +0200

    Add raw input styled component

--- src/utils/styled.components/index.tsx
+++ src/utils/styled.components/index.tsx
@@ -3,3 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 0 15px;
 `;
+
+export const Input = styled.input`
+  padding: 8px;
+  border-radius: 0;
+  border: 1px solid #000;
+  outline: none;
+  color: #000;
+`;
